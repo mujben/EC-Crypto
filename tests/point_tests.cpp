00e592f4550dbed978681e0911298c86ec676305
kh-lu2 <huhiak1@gmail.com>
2025-10-19 12:44:19 +0200

    more example tests

--- tests/point_tests.cpp
+++ tests/point_tests.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "../src/Point.cpp"
 
-TEST(point_tests, lhs_is_inf) {
+TEST(point_add_tests, lhs_is_inf) {
     EC curve;
     Point A(2, 3, false, curve);
     Point B(0, 0, true, curve);
